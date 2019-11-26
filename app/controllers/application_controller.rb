8f6b3f1e8684191efb75093d1d9cbd219134c623
Brandon Brasson <rocksong622@gmail.com>
2019-11-26 21:16:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ end
 
 post "/signup" do
   user = User.new(:username => params[:username], :password => params[:password])
- 
+
   if user.save
     redirect "/login"
   else
@@ -50,7 +50,7 @@ end
 
 	post "/login" do
 	  user = User.find_by(:username => params[:username])
-	 
+
 	  if user && user.authenticate(params[:password])
 	    session[:user_id] = user.id
 	    redirect "/success"
