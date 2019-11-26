f9f647468bba7f89c4ebf61e3c82200e2e0a1eac
Brandon Brasson <rocksong622@gmail.com>
2019-11-26 21:15:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,11 +16,11 @@ class ApplicationController < Sinatra::Base
 	    erb :signup
 	  end
 
-	
+
 
 post "/signup" do
   user = User.new(:username => params[:username], :password => params[:password])
- 
+
   if user.save
     redirect "/login"
   else
